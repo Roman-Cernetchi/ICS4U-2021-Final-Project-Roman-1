f98d8e99223d727be8c965911668f8a66e96defb
Ubuntu <ubuntu@ip-172-31-18-207.ec2.internal>
2022-01-26 17:32:47 +0000

    elements file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Tabletop from 'tabletop';
 
@@ -25,7 +24,6 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title"><font color="white">Company inventory tracker</font></h1>
         </header>
         </div>

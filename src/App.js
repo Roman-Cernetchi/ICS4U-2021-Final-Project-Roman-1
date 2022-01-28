1811f6ba03d02f4705c4a8b8c483c7e98e78da06
Ubuntu <ubuntu@ip-172-31-18-207.ec2.internal>
2022-01-28 03:07:11 +0000

    wrote code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import AddObject from "./components/AddObject";
+import Header from "./components/Header";
+import Objects from "./components/Objects";
+import "./styles.css";
 
-class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      data: []
-    }
+const getLocalStorage = () => {
+  let objects = localStorage.getItem("myObjects");
+  if (objects) {
+    return JSON.parse(localStorage.getItem("myObjects"));
+  } else {
+    return [];
   }
+};
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title"><font color="white">Company inventory tracker</font></h1>
-        </header>
-        </div>
-    );
-  }
+export default function App() {
+  const [showAddObject, setShowAddObject] = useState(false);
+  const [objects, setObjects] = useState(getLocalStorage());
+  // "inventory": [
+  //  {
+  //    "object": "Chair",
+  //    "quantity": 15,
+  //    "description": "4 legs with arm rests and green backrest",
+  //    "date of entry": "Jan 2nd, 2022"
+  // },
+  //  {
+  //    "object": "Table",
+  //    "quantity": 3,
+  //    "description": "Foldable grey table",
+  //    "date of entry": "Jan 13th, 2022"
+  //  },
+  //  {
+  //    "object": "Phone",
+  //    "quantity": 4,
+  //    "description": "IPhone X",
+  //   "date of entry": "Jan 23rd, 2022"
+  //  }
+  // ]
+
+  // Add Task
+  const addObject = (object) => {
+    const id = Math.floor(Math.random() * 10000 + 1);
+    const newObject = {
+      id: id,
+      ...object
+    };
+
+    setObjects([...objects, newObject]);
+  };
+
+  // Delete object
+  const deleteObject = (id) => {
+    setObjects(objects.filter((object) => object.id !== id));
+  };
+
+  // Show Form
+  const showObjectForm = () => {
+    setShowAddObject(!showAddObject);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("myObjects", JSON.stringify(objects));
+  }, [objects]);
+
+  return (
+    <div className="container">
+      <Header onAdd={showObjectForm} title="Inventory Tracker" showAdd={showAddObject} />
+      {showAddObject && <AddObject addObject={addObject} />}
+      {objects.length > 0 ? (
+        <Objects
+        objects={objects}
+          onDelete={deleteObject}
+          toggleReminder={toggleReminder}
+        />
+      ) : (
+        "No more objects"
+      )}
+    </div>
+  );
 }
-
-export default App;
